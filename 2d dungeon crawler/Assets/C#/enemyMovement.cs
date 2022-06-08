f92a727c868a555322229755a582d6b773a72de5
solarwaffles <sevie5@hotmail.com>
2022-06-08 15:14:56 -0400

    updated enemy movement

--- 2d dungeon crawler/Assets/C#/enemyMovement.cs
+++ 2d dungeon crawler/Assets/C#/enemyMovement.cs
@@ -6,8 +6,7 @@ public class enemyMovement : MonoBehaviour
 {
 
     public GameObject Enemy;
-    public float differenceInX;
-    public float differenceInY;
+    public float enemyMoveSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,29 +17,30 @@ public class enemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //makes the enemy move towards the player
+    //makes the enemy move towards the player
 
-        // differenceInX = PlayerInfo.playerLocation.x - Enemy.GetComponent<Transform>().position.x;
-        // differenceInY = PlayerInfo.playerLocation.y - Enemy.GetComponent<Transform>().position.y;
+        enemyMoveSpeed = (PlayerInfo.movementSpeed / 2);
 
+        //enemy moves in x direction towards player
         if (PlayerInfo.playerLocation.x > Enemy.GetComponent<Transform>().position.x)
         {
-            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x + (PlayerInfo.movementSpeed / 2), Enemy.GetComponent<Transform>().position.y);
+            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x + enemyMoveSpeed, Enemy.GetComponent<Transform>().position.y);
         }
 
         else if (PlayerInfo.playerLocation.x < Enemy.GetComponent<Transform>().position.x)
         {
-            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x - (PlayerInfo.movementSpeed / 2), Enemy.GetComponent<Transform>().position.y);
+            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x - enemyMoveSpeed, Enemy.GetComponent<Transform>().position.y);
         }
 
+        //enemy moves in y direction towards player
          if (PlayerInfo.playerLocation.y > Enemy.GetComponent<Transform>().position.y)
         {
-            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x, Enemy.GetComponent<Transform>().position.y + (PlayerInfo.movementSpeed / 2));
+            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x, Enemy.GetComponent<Transform>().position.y + enemyMoveSpeed);
         }
 
         else if (PlayerInfo.playerLocation.y < Enemy.GetComponent<Transform>().position.y)
         {
-            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x, Enemy.GetComponent<Transform>().position.y - (PlayerInfo.movementSpeed / 2));
+            transform.position = new Vector2 (Enemy.GetComponent<Transform>().position.x, Enemy.GetComponent<Transform>().position.y - enemyMoveSpeed);
         }
     }
 }
