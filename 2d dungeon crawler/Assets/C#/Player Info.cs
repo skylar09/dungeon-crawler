a93dd3b0a01b8757996aea6fa75cda476df88d24
solarwaffles <sevie5@hotmail.com>
2022-06-08 00:23:04 -0400

    Added player and player info script

---
+++ 2d dungeon crawler/Assets/C#/Player Info.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+
+    public static int playerHealth = 3;
+    public static int playerDamage = 5;
+    public static int playerDefense = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
