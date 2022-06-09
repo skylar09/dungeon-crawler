7394b1c5bfb982ef27d27da516564e6a8495c118
skylar09 <skylarbstrickler@gmail.com>
2022-06-09 19:31:37 -0400

    playermove

--- 2d dungeon crawler/Assets/C#/PlayerCollision.cs
+++ 2d dungeon crawler/Assets/C#/PlayerCollision.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,3 +26,35 @@ public class PlayerCollision : MonoBehaviour
         // }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void OnCollisionEnter2D (Collision2D collisionInfo)
+    {
+        //checks for the tag on the object it collides with
+        if (collisionInfo.collider.tag == "enemy")
+        {
+            //changes the player health
+            PlayerInfo.playerHealth = PlayerInfo.playerHealth - (Math.Abs(PlayerInfo.playerDefense - 4));
+
+            Debug.Log(PlayerInfo.playerHealth);
+        }
+
+        if (collisionInfo.collider.tag == "doorY")
+        {
+            //teleport player and change camera to new room
+            PlayerInfo.playerLocation = RoomLocation.room2;
+            Debug.Log(PlayerInfo.playerLocation);
+
+
+        }
+    }
+}
+>>>>>>> Stashed changes
---
+++ 2d dungeon crawler/Assets/C#/RoomLocation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLocation : MonoBehaviour
+{
+    public static Vector3 room1 = new Vector3(0, 0, -10);
+    public static Vector3 room2 = new Vector3(0, 11, -10);
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
