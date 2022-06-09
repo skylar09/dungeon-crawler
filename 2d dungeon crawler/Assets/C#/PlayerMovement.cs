6bd5385ac0aee60acd37aac8f2d4825829d26892
solarwaffles <sevie5@hotmail.com>
2022-06-08 22:20:15 -0400

    made enemy slower

--- 2d dungeon crawler/Assets/C#/PlayerMovement.cs
+++ 2d dungeon crawler/Assets/C#/PlayerMovement.cs
@@ -17,22 +17,22 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetKey("d"))
-        {            
+        {//moves player to the right            
             transform.position = new Vector2 (player.GetComponent<Transform>().position.x + PlayerInfo.movementSpeed, player.GetComponent<Transform>().position.y);
         }
 
         if (Input.GetKey("a"))
-        {
+        {//moves player to the left
             transform.position = new Vector2 (player.GetComponent<Transform>().position.x - PlayerInfo.movementSpeed, player.GetComponent<Transform>().position.y);
         }
 
         if (Input.GetKey("w"))
-        {
+        {//moves player up
             transform.position = new Vector2 (player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y + PlayerInfo.movementSpeed);
         }
 
         if (Input.GetKey("s"))
-        {
+        {//moves player down
             transform.position = new Vector2 (player.GetComponent<Transform>().position.x, player.GetComponent<Transform>().position.y - PlayerInfo.movementSpeed);
         }
     }
--- 2d dungeon crawler/Assets/C#/enemyInfo.cs
+++ 2d dungeon crawler/Assets/C#/enemyInfo.cs
@@ -8,7 +8,7 @@ public class enemyInfo : MonoBehaviour
     public static int enemyHealth = 1;
     public static int enemyDamage = 4;
     public static int enemyDefense = 2;
-    public static float enemyMoveSpeed = (PlayerInfo.movementSpeed / 2);
+    public static float enemyMoveSpeed = (PlayerInfo.movementSpeed / 3);
 
     // Start is called before the first frame update
     void Start()
