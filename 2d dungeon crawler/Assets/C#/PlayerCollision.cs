3bf1247e4ae50013018fa720f465c356f134fc3f
skylar09 <skylarbstrickler@gmail.com>
2022-06-09 19:54:26 -0400

    Update PlayerCollision.cs

--- 2d dungeon crawler/Assets/C#/PlayerCollision.cs
+++ 2d dungeon crawler/Assets/C#/PlayerCollision.cs
@@ -1,32 +1,3 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System;
-
-public class PlayerCollision : MonoBehaviour
-{
-
-
-    // Update is called once per frame
-    void OnCollisionEnter2D (Collision2D collisionInfo)
-    {
-        //checks for the tag on the object it collides with
-        if (collisionInfo.collider.tag == "enemy")
-        {
-            //changes the player health
-            PlayerInfo.playerHealth = PlayerInfo.playerHealth - (Math.Abs(PlayerInfo.playerDefense - 4));
-
-            Debug.Log(PlayerInfo.playerHealth);
-        }
-
-        // if (collisionInfo.collider.tag == "next room up")
-        // {
-            
-        // }
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -57,4 +28,4 @@ public class PlayerCollision : MonoBehaviour
         }
     }
 }
->>>>>>> Stashed changes
+
