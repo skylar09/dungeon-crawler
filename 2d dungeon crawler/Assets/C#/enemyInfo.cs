4db4868e239107ac0a8392a60483f8ffe0f7398c
solarwaffles <sevie5@hotmail.com>
2022-06-16 20:24:59 -0400

    Update enemyInfo.cs

--- 2d dungeon crawler/Assets/C#/enemyInfo.cs
+++ 2d dungeon crawler/Assets/C#/enemyInfo.cs
@@ -19,7 +19,11 @@ public class enemyInfo : MonoBehaviour
     public static float slimeMoveSpeed = 1;
     public static int slimeResistKnockback = 0;
 
-    
+    //makes array   note: to call a 2D array it goes vertical value then horizontal value --> [y, x]
+    public int[,] enemyStats = {
+        {1, 1, 1, 0},
+        {4, 2, 2, 0}
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +34,6 @@ public class enemyInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         //makes enemy disappear if it's dead
         if (batHealth <= 0)
         {
