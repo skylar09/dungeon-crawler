e5db84a1728762961fa5c66cbabbc9c001171f48
solarwaffles <sevie5@hotmail.com>
2022-09-03 13:38:29 -0400

    made credits screen

--- 2d dungeon crawler/Assets/C#/MainMenu/Options_Credits.cs
+++ 2d dungeon crawler/Assets/C#/MainMenu/Options_Credits.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Options : MonoBehaviour
+public class Options_Credits : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject optionsMenu;
+    public GameObject creditScreen;
     // public GameObject start;
 
     public void openOptions()
@@ -18,5 +19,17 @@ public class Options : MonoBehaviour
     {
         optionsMenu.SetActive(false);
         mainMenu.SetActive(true);
+    }
+
+    public void startCredits()
+    {
+        mainMenu.SetActive(false);
+        creditScreen.SetActive(true);
+    }
+
+    public void endCredits()
+    {
+        mainMenu.SetActive(true);
+        creditScreen.SetActive(false);
     }
 }
