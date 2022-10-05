a94199d0f2c15e23b79a3db842d9bf81d1279108
SSchulhoff <sevie5@hotmail.com>
2022-10-04 20:30:17 -0400

    finished new enemy

--- 2d dungeon crawler/Assets/C#/Enemies/enemyMovement.cs
+++ 2d dungeon crawler/Assets/C#/Enemies/enemyMovement.cs
@@ -4,38 +4,23 @@ using UnityEngine;
 
 public class enemyMovement : MonoBehaviour
 {
-    private Transform target;
     public Rigidbody2D rb;
     public bool canMove = true;
     public bool closeTo = false;
-    public bool meleeAttack = false;
     public float moveSpeed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        //sets target as the GameObject with the tag "Player"
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-    }
 
     // FixedUpdate is called once per set amount of frames
     void FixedUpdate()
     {
         //checks if enemy is within a certain distance of the player
-        if (Vector2.Distance(transform.position, target.position) < 6)
+        if (Vector2.Distance(transform.position, PlayerInfo.playerLocation) < 6)
         {
             closeTo = true;
-            if (Vector2.Distance(transform.position, target.position) < 2)
-            {
-                meleeAttack = true;
-            }
-            else{
-                meleeAttack = false;
-            }
+            
             //makes the enemy move towards the player
             if (canMove == true)
             {
-                transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                transform.position = Vector2.MoveTowards(transform.position, PlayerInfo.playerLocation, moveSpeed * Time.deltaTime);
             }
                     
         }
--- 2d dungeon crawler/Assets/C#/Enemies/golgoblin/nearbyAttackAnim.cs
+++ 2d dungeon crawler/Assets/C#/Enemies/golgoblin/nearbyAttackAnim.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class nearbyAttackAnim : MonoBehaviour
 {
     public Animator animator;
+    public bool meleeAttack = false;
+    public double distance;
 
     void Update()
     {
-        if (this.GetComponent<enemyMovement>().meleeAttack == true)
+        if (meleeAttack == true)
         {
             //this.GetComponent<enemyMovement>().canMove = false;
             animator.SetBool("playerNearby", true);
@@ -17,6 +19,18 @@ public class nearbyAttackAnim : MonoBehaviour
         {
             //this.GetComponent<enemyMovement>().canMove = true;
             animator.SetBool("playerNearby", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector2.Distance(transform.position, PlayerInfo.playerLocation) < distance)
+        {
+            meleeAttack = true;
+        }
+        else
+        {
+            meleeAttack = false;
         }
     }
     
