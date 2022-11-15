0e7d64e58c50f993ab1bd2433a6079c1a5ad5876
SSchulhoff <sevie5@hotmail.com>
2022-11-15 18:13:58 -0500

    made it so weapon moves in small circle on player

--- 2d dungeon crawler/Assets/C#/Weapons/weaponRotate.cs
+++ 2d dungeon crawler/Assets/C#/Weapons/weaponRotate.cs
@@ -5,37 +5,36 @@ using UnityEngine;
 public class weaponRotate : MonoBehaviour
 {
     Vector3 mouse_pos;
-    Transform target; //Assign to the object you want to rotate
     public static Vector3 object_pos;
     float angle;
  
-    public void Start()
-    {
-        target = this.transform;
-    }
-
-    public void Update()
-    {
-        if (Weapons.swordSwung != true)
-        {
-             mouse_pos = Input.mousePosition;
-            //mouse_pos.z = 10; //The distance between the camera and object
-            object_pos = Camera.main.WorldToScreenPoint(target.position);
-            mouse_pos.x = mouse_pos.x - object_pos.x;
-            mouse_pos.y = mouse_pos.y - object_pos.y;
-            angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-
-            if (mouse_pos.x > 0)
-                transform.rotation = Quaternion.Euler(new Vector3(0, 180, 90 - angle));
-            else
-                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    public void Update(){
+        if (Weapons.swordSwung != true){
+            rotate();
         }
 
-        // float posX = PlayerInfo.playerLocation.x - Mathf.Cos(angle) * .15f;
-        // float posY = PlayerInfo.playerLocation.y - Mathf.Sin(angle) * .15f;
-
-        // transform.position = new Vector3(posX, posY, 0);
-        transform.position = PlayerInfo.playerLocation;
+        setPosition();        
     } 
+
+    public void rotate(){
+        mouse_pos = Input.mousePosition;
+        object_pos = Camera.main.WorldToScreenPoint(PlayerInfo.playerLocation);
+        //Debug.Log(object_pos);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
+        if (mouse_pos.x > 0)
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 90 - angle));
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+    }
+
+    public void setPosition(){
+        float posX = PlayerInfo.playerLocation.x + (Mathf.Cos(angle * Mathf.PI / 180) * .3f);
+        float posY = PlayerInfo.playerLocation.y + (Mathf.Sin(angle * Mathf.PI / 180) * .3f);
+        transform.position = new Vector3(posX, posY, 0);
+    }
 }
