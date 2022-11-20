2e9e9a7c0a28865555c9e6410707f5e614b1e2b8
SSchulhoff <sevie5@hotmail.com>
2022-11-19 23:46:39 -0500

    imroved gun

--- 2d dungeon crawler/Assets/C#/Weapons/shootProjectile.cs
+++ 2d dungeon crawler/Assets/C#/Weapons/shootProjectile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class shootProjectile : MonoBehaviour
 {
@@ -8,9 +9,23 @@ public class shootProjectile : MonoBehaviour
     float posX, posY;
     public GameObject projectile;
     
+    void Update()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject()){
+            //if left click
+            if (Input.GetMouseButtonDown(0) && Weapons.canAttack == true)
+            {
+                spawnProjectile();
+            }
+        }
+    }
+
     public void spawnProjectile(){
         posX = this.transform.parent.gameObject.transform.position.x + (weaponRotate.cos * dist);
         posY = this.transform.parent.gameObject.transform.position.y + (weaponRotate.sin * dist);
         GameObject bla = Instantiate(projectile, new Vector3(posX, posY, 0), Quaternion.AngleAxis(weaponRotate.angle, Vector3.forward));
+
+        Weapons.canAttack = true;
+        Weapons.swordSwung = false;
     }
 }
